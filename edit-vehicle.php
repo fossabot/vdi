ab115871296c57fde1f501f1887095689ea3e2c7
chssn <chris529@gmail.com>
2018-04-02 21:58:15 +0100

    Major update for #12
    This update will allow level 3 users (DLO) to hide vehicles from the
    edit screen and for level 4 users (admin) to restore or unhide vehicles
    from the edit screen.

--- edit-vehicle.php
+++ edit-vehicle.php
@@ -1,7 +1,7 @@
 <?php
 require 'include/login-check.php';
 require 'functions/functions.php';
-check_auth(4); // 1 = all users, 2 = supervisor, 3 = DLO & 4 = admin
+check_auth(3); // 1 = all users, 2 = supervisor, 3 = DLO & 4 = admin
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,12 +21,13 @@ check_auth(4); // 1 = all users, 2 = supervisor, 3 = DLO & 4 = admin
   require 'include/header.php';
   require 'include/sql-connect.php';
   ?>
-  <br />
-  <div class="w3-container">
-    Deleted (hidden) vehicles are highlighted in orange at the bottom of the page. Currently only hidden or unhidden by manual edit in phpMyAdmin.
-  </div>
+  <br /><br />
   <div class="w3-container">
     <table class="w3-table-all w3-hoverable">
+      <?php
+      if($_SESSION['role'] == 3) { ?>
+        <tr><th colspan="3"></th><th class="w3-center w3-blue" colspan="3">Editable Fields</th><th colspan="3"></th></tr>
+      <?php } ?>
       <tr>
         <th>Callsign</th>
         <th>Vehicle Type</th>
@@ -37,6 +38,7 @@ check_auth(4); // 1 = all users, 2 = supervisor, 3 = DLO & 4 = admin
         <th>ISSI HH1</th>
         <th>ISSI HH2</th>
         <th>ISSI VEH</th>
+        <th>Delete</th>
       </tr>
       <?php
       //put vehicle types into x-editable array
@@ -63,33 +65,67 @@ check_auth(4); // 1 = all users, 2 = supervisor, 3 = DLO & 4 = admin
       }
       $veh_status = "[".$veh_status."]";
 
-      //select all vehicles
-      $sqla = "SELECT vehicle_list.id, vehicle_list.callsign, vehicle_types.vehicle_type, vehicle_list.registration, IF(vehicle_list.mot = 0,NULL,FROM_UNIXTIME(vehicle_list.mot, '%d/%m/%Y')) AS mot, IF(vehicle_list.service = 0,NULL,FROM_UNIXTIME(vehicle_list.service, '%d/%m/%Y')) AS service,";
-      $sqlb = "vehicle_status.vehicle_status, vehicle_list.issi_hh1, vehicle_list.issi_hh2, vehicle_list.issi_veh, vehicle_list.hidden FROM vehicle_list";
-      $sqlc = "LEFT JOIN vehicle_types ON vehicle_list.vehicle_type = vehicle_types.id LEFT JOIN vehicle_status ON vehicle_list.veh_status = vehicle_status.id ORDER BY hidden ASC, callsign ASC";
-      $sql = "$sqla $sqlb $sqlc";
-      $result = $conn->query($sql);
+      if($_SESSION['role'] == 3) { //section for DLOs
+        //select all vehicles
+        $sqla = "SELECT vehicle_list.id, vehicle_list.callsign, vehicle_types.vehicle_type, vehicle_list.registration, IF(vehicle_list.mot = 0,NULL,FROM_UNIXTIME(vehicle_list.mot, '%d/%m/%Y')) AS mot, IF(vehicle_list.service = 0,NULL,FROM_UNIXTIME(vehicle_list.service, '%d/%m/%Y')) AS service,";
+        $sqlb = "vehicle_status.vehicle_status, vehicle_list.issi_hh1, vehicle_list.issi_hh2, vehicle_list.issi_veh FROM vehicle_list";
+        $sqlc = "LEFT JOIN vehicle_types ON vehicle_list.vehicle_type = vehicle_types.id LEFT JOIN vehicle_status ON vehicle_list.veh_status = vehicle_status.id WHERE hidden = 0 ORDER BY callsign ASC";
+        $sql = "$sqla $sqlb $sqlc";
+        $result = $conn->query($sql);
 
-			if ($result->num_rows > 0) {
-				while($row = $result->fetch_assoc()) {
-          if ($row['hidden'] != 0) {
-            $col = "class='w3-orange'";
-          } else {
-            $col = NULL;
+  			if ($result->num_rows > 0) {
+  				while($row = $result->fetch_assoc()) {
+            $hash = hash('sha256', $row['callsign']."-".$row['registration']."-".$row['id']."-".$salt); //create sha256 hash to remove a vehicle from display
+  					echo'<tr>
+                  <td>'.$row['callsign'].'</td>
+    							<td>'.$row['vehicle_type'].'</td>
+    							<td>'.$row['registration'].'</td>
+                  <td class="xedit" data-type="date" data-clear="1" data-pk="'.$row['id'].'" data-name="mot" data-format="dd-mm-yyyy" data-viewformat="dd/mm/yyyy">'.$row['mot'].'</td>
+                  <td class="xedit" data-type="date" data-clear="1" data-pk="'.$row['id'].'" data-name="service" data-format="dd-mm-yyyy" data-viewformat="dd/mm/yyyy">'.$row['service'].'</td>
+                  <td class="xedit" data-type="select" data-source="'.$veh_status.'" data-pk="'.$row['id'].'" data-name="veh_status">'.$row['vehicle_status'].'</td>
+                  <td>'.$row['issi_hh1'].'</td>
+                  <td>'.$row['issi_hh2'].'</td>
+                  <td>'.$row['issi_veh'].'</td>
+                  <td><button class="w3-button" onclick="deleteRecord(`'.$hash.'`,`'.$row['callsign'].'`)"><i class="fas fa-minus-circle"></i></button></td>
+                </tr>';
+  			    }
           }
-					echo'<tr '.$col.'>
-                <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="callsign">'.$row['callsign'].'</td>
-  							<td class="xedit" data-type="select" data-source="'.$types.'" data-pk="'.$row['id'].'" data-name="vehicle_type">'.$row['vehicle_type'].'</td>
-  							<td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="registration">'.$row['registration'].'</td>
-                <td class="xedit" data-type="date" data-clear="1" data-pk="'.$row['id'].'" data-name="mot" data-format="dd-mm-yyyy" data-viewformat="dd/mm/yyyy">'.$row['mot'].'</td>
-                <td class="xedit" data-type="date" data-clear="1" data-pk="'.$row['id'].'" data-name="service" data-format="dd-mm-yyyy" data-viewformat="dd/mm/yyyy">'.$row['service'].'</td>
-                <td class="xedit" data-type="select" data-source="'.$veh_status.'" data-pk="'.$row['id'].'" data-name="veh_status">'.$row['vehicle_status'].'</td>
-                <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="issi_hh1">'.$row['issi_hh1'].'</td>
-                <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="issi_hh2">'.$row['issi_hh2'].'</td>
-                <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="issi_veh">'.$row['issi_veh'].'</td>
-              </tr>';
-			    }
-        }
+        } elseif($_SESSION['role'] == 4) { //section for admin
+          //select all vehicles
+          $sqla = "SELECT vehicle_list.id, vehicle_list.callsign, vehicle_types.vehicle_type, vehicle_list.registration, IF(vehicle_list.mot = 0,NULL,FROM_UNIXTIME(vehicle_list.mot, '%d/%m/%Y')) AS mot, IF(vehicle_list.service = 0,NULL,FROM_UNIXTIME(vehicle_list.service, '%d/%m/%Y')) AS service,";
+          $sqlb = "vehicle_status.vehicle_status, vehicle_list.issi_hh1, vehicle_list.issi_hh2, vehicle_list.issi_veh, vehicle_list.hidden FROM vehicle_list";
+          $sqlc = "LEFT JOIN vehicle_types ON vehicle_list.vehicle_type = vehicle_types.id LEFT JOIN vehicle_status ON vehicle_list.veh_status = vehicle_status.id ORDER BY hidden ASC, callsign ASC";
+          $sql = "$sqla $sqlb $sqlc";
+          $result = $conn->query($sql);
+
+    			if ($result->num_rows > 0) {
+    				while($row = $result->fetch_assoc()) {
+              $hash = hash('sha256', $row['callsign']."-".$row['registration']."-".$row['id']."-".$salt); //create sha256 hash to remove a vehicle from display
+              if ($row['hidden'] != 0) {
+                $col = "class='w3-orange'";
+              } else {
+                $col = NULL;
+              }
+    					echo'<tr '.$col.'>
+                    <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="callsign">'.$row['callsign'].'</td>
+      							<td class="xedit" data-type="select" data-source="'.$types.'" data-pk="'.$row['id'].'" data-name="vehicle_type">'.$row['vehicle_type'].'</td>
+      							<td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="registration">'.$row['registration'].'</td>
+                    <td class="xedit" data-type="date" data-clear="1" data-pk="'.$row['id'].'" data-name="mot" data-format="dd-mm-yyyy" data-viewformat="dd/mm/yyyy">'.$row['mot'].'</td>
+                    <td class="xedit" data-type="date" data-clear="1" data-pk="'.$row['id'].'" data-name="service" data-format="dd-mm-yyyy" data-viewformat="dd/mm/yyyy">'.$row['service'].'</td>
+                    <td class="xedit" data-type="select" data-source="'.$veh_status.'" data-pk="'.$row['id'].'" data-name="veh_status">'.$row['vehicle_status'].'</td>
+                    <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="issi_hh1">'.$row['issi_hh1'].'</td>
+                    <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="issi_hh2">'.$row['issi_hh2'].'</td>
+                    <td class="xedit" data-type="text" data-clear="1" data-pk="'.$row['id'].'" data-name="issi_veh">'.$row['issi_veh'].'</td>';
+
+                if ($row['hidden'] == 1) { //if hidden allow to add
+                  echo '<td><button class="w3-button" onclick="restoreRecord(`'.$hash.'`,`'.$row['callsign'].'`)"><i class="fas fa-plus-circle"></i></button></td>';
+                } elseif ($row['hidden'] == 0) { //if showing allow to remove
+                  echo '<td><button class="w3-button" onclick="deleteRecord(`'.$hash.'`,`'.$row['callsign'].'`)"><i class="fas fa-minus-circle"></i></button></td>';
+                }
+                echo "</tr>";
+              }
+  			    }
+          }
 			?>
     </table>
   </div>
@@ -147,6 +183,18 @@ check_auth(4); // 1 = all users, 2 = supervisor, 3 = DLO & 4 = admin
         });
       });
     });
+
+    //delete confirmation
+    function deleteRecord(hash,cs) {
+      if (confirm('Are you sure you want to delete '+cs+'?')) {
+        location.href='include/edit-hide-vehicle.php?f=' + hash;
+      }
+    }
+    function restoreRecord(hash,cs) {
+      if (confirm('Are you sure you want to restore '+cs+'?')) {
+        location.href='include/edit-hide-vehicle.php?r&f=' + hash;
+      }
+    }
 </script>
 </body>
 </html>
---
+++ include/edit-hide-vehicle.php
@@ -0,0 +1,23 @@
+<?php
+require 'login-check.php';
+require '../functions/functions.php';
+check_auth(3); // 1 = all users, 2 = supervisor, 3 = DLO & 4 = admin
+
+if (isset($_GET['f'])) {
+  require 'sql-connect.php';
+  $hash_rx = $_GET['f'];
+
+  if (isset($_GET['r'])) { //section to allow a vehicle to be un-hidden
+    $hide = 0;
+  } else {
+    $hide = 1;
+  }
+  $sql = "UPDATE vehicle_list SET hidden = $hide WHERE SHA2(CONCAT_WS('-',callsign,registration,id,'$salt'), 256) = '$hash_rx' LIMIT 1";
+
+  if($conn->query($sql) != TRUE) {
+    die("Error: $sql <br />" . $conn->error);
+  }
+
+  header("Location: ".$_SERVER['HTTP_REFERER']);
+}
+?>
