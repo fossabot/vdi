969daa9d26041056e786393bb46a544d492a115b
chssn <chris529@gmail.com>
2018-04-10 21:11:10 +0100

    eVIMS branding update

--- include/scripts.php
+++ include/scripts.php
@@ -1,4 +1,4 @@
-  <title>Vehicle Daily Inspection</title>
+  <title>eVIMS</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
