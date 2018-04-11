027d44695f95bf32f0a40f056ec3a003e77ce88b
chssn <chris529@gmail.com>
2018-04-11 20:55:50 +0100

    Fix #24 - modifications made to user security session

--- include/login-check.php
+++ include/login-check.php
@@ -3,44 +3,34 @@ session_start();
 if(!isset($_COOKIE['vdiuser'])) { // if no cookie is set then go to the login page
   header('Location: login.php');
   exit;
-} else {  // if a cookie is set...
+} elseif (isset($_COOKIE['vdiuser'])) {  // if a cookie is set...
   $ck_check = $_COOKIE['vdiuser'];
   //see if the cookie data is valid
   require 'sql-connect.php';
-  $sql = "SELECT * FROM users WHERE session_key = '$ck_check' LIMIT 1";
+  $sql = "SELECT id FROM users WHERE session_key = '$ck_check' LIMIT 1";
   $result = $conn->query($sql);
 
-  if ($result->num_rows > 0) {
+  if ($result->num_rows == 1) { //does the cookie match the db?
     //check if session exists
     if (!isset($_SESSION['key'])) {
       header('Location: login.php');
       exit;
-    } else {
-      // continue with page
+    } elseif ($_SESSION['key'] === $ck_check) { //does the session match the cookie?
+      $pass = 1;
     }
-
-    // reset one time hash
-    // set the user cookie
-    /*require '/vdi/include/ppp.php';
-    $cookie_name = "vdiuser";
-    $cookie_value = GenerateRandomSequenceKey();
-    $cookie_expire = 0; // cookie expires when the browser closes
-    setcookie($cookie_name, $cookie_value, $cookie_expire,"/vdi/", "vremote.theparkys.net", 1, 1);
-
-    // update user record with cookie value
-    $sql = "UPDATE users SET session_key = '$cookie_value' WHERE staff_number = '" . $_SESSION['staff_number'] . "' LIMIT 1";
-
-    if ($conn->query($sql) === TRUE) {
-      //update session key value
-      $_SESSION['key'] = $cookie_value;
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-      exit;
-    }*/
   } else {
     header('Location: login.php');
     exit;
   }
+}
+if (!isset($pass)) {
+  header('Location: login.php');
+  exit;
+} elseif ($pass == 1) {
+  //continue...
+} else {
+  header('Location: login.php');
+  exit;
 }
 $conn->close();
 ?>
